28db00958a6305ef193aa208408ea9dfc5bb7ffd
cwoodward10 <c.woodward10@gmail.com>
2018-03-25 11:29:08 -0500

    Update comments and doc strings per style guide

--- entertainment_center.py
+++ entertainment_center.py
@@ -5,23 +5,23 @@ import fresh_tomatoes
 import media
 import csv
 
-#create empty list that will eventually contain all the movie_poster_url
+#fresh_tomatoes requires a list of movies
 movies_list = []
 
-#import information from the CSV file
-#chose to use a CSV file to store the information about the movies
-#this way, I do not muddle up this code with too many class creations
-#documentation here: https://docs.python.org/3.5/library/csv.html
+
+#Chose to use a CSV file to store the information about the movies.
+#This way, I do not muddle up this code with too many class creations
+#but instead just appended each row of the csv file to the movies_list.
+
 with open('Movies_CSV.csv') as  csvfile:
     reader = csv.DictReader(csvfile)
-    #iterate over each movie in the csv file
+
     for movie in reader:
         title = str(movie['Movie Title'])
         poster = str(movie['Movie Poster URL'])
         trailer = str(movie['Movie Trailer URL'])
-        #movie_id is what shows up if printing the movies_list
-        movie_id = str(movie['Movie ID'])
-        #append the movies_list with 1 class instance per movie in csv
+        movie_id = str(movie['Movie ID']) #name as appears in movies_list
+
         movies_list.append(media.Movie(title, poster, trailer, movie_id))
 
 #run to create the HTML file for the movies webpage
--- media.py
+++ media.py
@@ -2,22 +2,23 @@
 
 class Movie():
     '''Class that contains general information about a movie.
-        Formatted in such a way that it can be called by the
+
+        The class is formatted in such a way that it can be called by the
         fresh_tomatoes.py file and thus dispalyed in the html file'''
 
-    #init file to gather all the information called by theme
-    #create_movie_tiles_content function in fresh_tomatoes.py
+
     def __init__(self, movie_title, movie_poster_url, movie_trailer_url, movie_id):
+        '''Inits class movie '''
         self.title = movie_title
         self.poster_image_url = movie_poster_url #link to an image file
         self.trailer_youtube_url = movie_trailer_url #link to a trailer on youtube
-        self.movie_id = movie_id
+        self.movie_id = movie_id #added so that a name can be dynamically assigned
+                                 #when pulled from the  csv file
 
-    #give this a readable name so people know what it is if its printed
     def __str__(self):
         name_string = "Movie Object with information about {}".format(self.title)
         return name_string
 
-    #give it a callable name
     def __repr__(self):
+        '''used so class can be dynamically named using movie_id'''
         return self.movie_id
