0258e5d62eceb59cbe72395c3149436e8e12acc5
cwoodward10 <c.woodward10@gmail.com>
2018-03-25 10:31:11 -0500

    add __repr__ to the movie class

--- media.py
+++ media.py
@@ -7,12 +7,17 @@ class Movie():
 
     #init file to gather all the information called by theme
     #create_movie_tiles_content function in fresh_tomatoes.py
-    def __init__(self, movie_title, movie_poster_url, movie_trailer_url):
+    def __init__(self, movie_title, movie_poster_url, movie_trailer_url, movie_id):
         self.title = movie_title
         self.poster_image_url = movie_poster_url #link to an image file
         self.trailer_youtube_id = movie_trailer_url #link to a trailer on youtube
+        self.movie_id = movie_id
 
     #give this a readable name so people know what it is if its printed
     def __str__(self):
         name_string = "Movie Object with information about {}".format(self.title)
         return name_string
+
+    #give it a callable name
+    def __repr__(self):
+        return self.movie_id
