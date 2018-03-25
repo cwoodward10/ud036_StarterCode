b37b4ee5634e693d2390755267622e9e835d0f50
cwoodward10 <c.woodward10@gmail.com>
2018-03-25 18:37:26 -0500

    Refine for pep8 guidelines

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,28 +1,28 @@
-#note: used python 3.5 to write this, idk if its that different from 2.7
+# note: used python 3.5 to write this, idk if its that different from 2.7
 
-#import these modules
+# import these modules
 import fresh_tomatoes
 import media
 import csv
 
-#fresh_tomatoes requires a list of movies
+# fresh_tomatoes requires a list of movies
 movies_list = []
 
 
-#Chose to use a CSV file to store the information about the movies.
-#This way, I do not muddle up this code with too many class creations
-#but instead just appended each row of the csv file to the movies_list.
+# Chose to use a CSV file to store the information about the movies.
+# This way, I do not muddle up this code with too many class creations
+# but instead just appended each row of the csv file to the movies_list.
 
-with open('Movies_CSV.csv') as  csvfile:
+with open('Movies_CSV.csv') as csvfile:
     reader = csv.DictReader(csvfile)
 
     for movie in reader:
         title = str(movie['Movie Title'])
         poster = str(movie['Movie Poster URL'])
         trailer = str(movie['Movie Trailer URL'])
-        movie_id = str(movie['Movie ID'])  #name as appears in movies_list
+        movie_id = str(movie['Movie ID'])  # name as appears in movies_list
 
         movies_list.append(media.Movie(title, poster, trailer, movie_id))
 
-#run to create the HTML file for the movies webpage
+# run to create the HTML file for the movies webpage
 fresh_tomatoes.open_movies_page(movies_list)
--- media.py
+++ media.py
@@ -1,4 +1,5 @@
-#Contains a set of classes related to media such as movies
+# Contains a set of classes related to media such as movies
+
 
 class Movie():
     '''Class that contains general information about a movie.
@@ -6,17 +7,16 @@ class Movie():
         The class is formatted in such a way that it can be called by the
         fresh_tomatoes.py file and thus dispalyed in the html file'''
 
-
-    def __init__(self, movie_title, movie_poster_url, movie_trailer_url, movie_id):
+    def __init__(self, movie_title, movie_poster_url,
+                 movie_trailer_url, movie_id):
         '''Inits class movie '''
         self.title = movie_title
-        self.poster_image_url = movie_poster_url  #link to an image file
-        self.trailer_youtube_url = movie_trailer_url  #link to a trailer on youtube
-        self.movie_id = movie_id  #added so that a name can be dynamically assigned
-                                  #when pulled from the  csv file
+        self.poster_image_url = movie_poster_url  # url of image file
+        self.trailer_youtube_url = movie_trailer_url  # url of youtube trailer
+        self.movie_id = movie_id  # dynamically assigned name of instance
 
     def __str__(self):
-        name_string = "Movie Object with information about {}".format(self.title)
+        name_string = "Movie Object containing {}".format(self.title)
         return name_string
 
     def __repr__(self):
